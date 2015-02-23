ab8b59ac939989ab7a0370cae56b9a379833aae6
Dennis <dj@dennisjefferson.com>
2015-02-22 19:09:10 -0800

    Clean up

--- ontra/ontra/ontra.js
+++ ontra/ontra/ontra.js
@@ -1,10 +1,11 @@
-// +_Line - logo idea: it would be a line with a plus representing the core ui
+
 Posts = new Mongo.Collection("posts");
 var groundPosts = new Ground.Collection(Posts);
 Users = new Mongo.Collection("users");
 var groundUsers = new Ground.Collection(Users);
 
 if (Meteor.isClient) {
+
   Meteor.subscribe("posts");
   
   Template.body.events({
@@ -27,19 +28,8 @@ if (Meteor.isClient) {
   });
   
   Template.body.helpers({
-  
     posts: function () {
-      return Posts.find({}); // Return all documents from Mongo "Task" collection
-    }
-  });
-
-  Template.post.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Posts.update(this._id, {$set: {checked: ! this.checked}});      
-    },
-    "click .delete": function () {
-      Posts.remove(this._id);
+      return Posts.find({}); // Return all documents from Mongo "Posts" collection
     }
   });
 
@@ -50,21 +40,40 @@ if (Meteor.isClient) {
 Meteor.methods({
   'fetchJSONData': function() {
     
-
     var postsResponse = Meteor.http.call("GET","https://raw.githubusercontent.com/djfrsn/frontend-interview-test-long/master/codetest/data/posts.json");
     var usersResponse = Meteor.http.call("GET","https://raw.githubusercontent.com/djfrsn/frontend-interview-test-long/master/codetest/data/users.json");
-    console.log(postsResponse);
-    console.log(usersResponse);
-}
+    
+    var postsData = JSON.parse(postsResponse.content);
+
+    var usersData = JSON.parse(usersResponse.content);
+
+    console.log(postsData);
+    console.log(usersData);
+
+    Posts.insert({
+      postsData: postsData,
+      date: new Date() // current time
+    });
+
+    Users.insert({
+      usersData: usersData,
+      date: new Date() // current time
+    });
+
+  }
 });
 
 
-Meteor.publish('posts', function(){
-  return Posts.find();
-});
+
 
 if (Meteor.isServer) {
+  
+  Meteor.publish('posts', function(){
+    return Posts.find();
+  });
+
   Meteor.startup(function () {
     Meteor.call("fetchJSONData");
   });
+  
 }
